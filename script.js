44170a3cc7e7bcd4377e55ca39985b8350701d2e
AhmadMCer <ahmadmcer@gmail.com>
2021-10-06 13:59:30 +0700

    ADD: Mobile view.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let menuList = document.getElementById('menuList');
+let nav = document.getElementsByTagName('nav')[0];
+let screenMobile = window.matchMedia('(max-width: 768px)');
+let menuToggle = false;
 
 for (let li of menuList.children) {
   li.firstElementChild.addEventListener('click', function() {
@@ -6,19 +9,35 @@ for (let li of menuList.children) {
   });
 }
 
+for (let i of menuList.children) {
+  for (let j of i.children[1].children) {
+    j.firstElementChild.addEventListener('click', menuDropdown);
+    j.firstElementChild.addEventListener('click', function() {
+      for (let k of menuList.children) {
+        k.classList.remove('sub-on');
+      }
+    });
+  }
+}
+
 function menuSearch() {
   let input, filter, lisTitle, lisSubTitle, title, subTitle, found, i, j;
-  input = document.getElementById('menuSearch');
+  input = document.getElementById('menuSearch').firstElementChild;
   filter = input.value.toLowerCase();
   lisTitle = menuList.children;
 
   if (input.value.length > 0) {
-    for (li of lisTitle) {
-      li.classList.add('sub-on');
+    for (i of lisTitle) {
+      i.classList.add('sub-on');
     }
   } else {
-    for (li of lisTitle) {
-      li.classList.remove('sub-on');
+    for (i of lisTitle) {
+      i.classList.remove('sub-on');
+      lisSubTitle = i.children[1].children;
+
+      for (j of lisSubTitle) {
+        j.style.display = '';
+      }
     }
   }
 
@@ -41,14 +60,22 @@ function menuSearch() {
       }
     }
 
-    for (j of lisSubTitle) {
-      j.style.display = '';
-    }
-
     if (found) {
       i.style.display = '';
     } else {
       i.style.display = 'none';
+    }
+  }
+}
+
+function menuDropdown() {
+  if (screenMobile.matches) {
+    if (!menuToggle) {
+      nav.style.height = '100%';
+      menuToggle = true;
+    } else {
+      nav.style.height = '13%';
+      menuToggle = false;
     }
   }
 }
